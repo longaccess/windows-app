de5f787e62260db14c50a97eb84f72a411b0f7fb
ShadowMech <basilis.lab@gmail.com>
2014-01-10 12:12:28 +0200

    Updated icons

--- GuiClient/MainForm.Designer.cs
+++ GuiClient/MainForm.Designer.cs
@@ -1134,6 +1134,7 @@
             this.Controls.Add(this.tabPageContainer);
             this.Controls.Add(this.menuStrip1);
             this.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "MainForm";
             this.Text = "LongAccess";
